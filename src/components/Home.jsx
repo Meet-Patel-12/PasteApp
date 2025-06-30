6ca036b167adbd3dfaef53957daa5c1f998ae913
Meet Patel <meetsinga127@gmail.com>
2025-06-30 22:11:15 +0530

    This is my second commit after testing the app and de-bug the problems

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { addToPastes, updateToPastes } from "../features/pasteSlice";
+import toast from "react-hot-toast";
 
 const Home = () => {
   const [title, setTitle] = useState("");
@@ -20,48 +21,52 @@ const Home = () => {
   }, [pasteId]);
 
   function createPaste() {
-    const paste = {
-      title: title,
-      content: value,
-      _id: pasteId || Date.now().toString(36),
-      createdAt: new Date().toISOString(),
-    };
-    // Add your dispatch or logic here
-    if (pasteId) {
-      dispatch(updateToPastes(paste));
+    if (title === "" || value === "") {
+      toast.error("Please Fill all the Field!");
     } else {
-      dispatch(addToPastes(paste));
-    }
+      const paste = {
+        title: title,
+        content: value,
+        _id: pasteId || Date.now().toString(36),
+        createdAt: new Date().toISOString(),
+      };
+      // Add your dispatch or logic here
+      if (pasteId) {
+        dispatch(updateToPastes(paste));
+      } else {
+        dispatch(addToPastes(paste));
+      }
 
-    setTitle("");
-    setValue("");
-    setSearchParams({});
+      setTitle("");
+      setValue("");
+      setSearchParams({});
+    }
   }
 
   return (
     <div className="min-h-screen w-full bg-white p-8">
-        <div className="flex flex-col md:flex-row justify-between gap-3 mb-5">
-          <input
-            className="flex-1 p-3 rounded-2xl border-2 border-blue-400 focus:outline-none focus:ring-2 focus:ring-blue-400"
-            type="text"
-            placeholder="Enter Title Here"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-          />
-          <button
-            onClick={createPaste}
-            className=" bg-blue-600 text-white font-semibold px-6 py-3 rounded-2xl hover:bg-blue-700 transition"
-          >
-            {pasteId ? "Update Paste" : "Create Paste"}
-          </button>
-        </div>
-        <textarea
-          className="w-full p-3 rounded-lg border-2 border-blue-400 bg-white focus:outline-none focus:ring-2 focus:ring-blue-400"
-          value={value}
-          placeholder="Enter Content Here"
-          onChange={(e) => setValue(e.target.value)}
-          rows={20}
+      <div className="flex flex-col md:flex-row justify-between gap-3 mb-5">
+        <input
+          className="flex-1 p-3 rounded-2xl border-2 border-blue-400 focus:outline-none focus:ring-2 focus:ring-blue-400"
+          type="text"
+          placeholder="Enter Title Here"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
         />
+        <button
+          onClick={createPaste}
+          className=" bg-blue-600 text-white font-semibold px-6 py-3 rounded-2xl hover:bg-blue-700 transition"
+        >
+          {pasteId ? "Update Paste" : "Create Paste"}
+        </button>
+      </div>
+      <textarea
+        className="w-full p-3 rounded-lg border-2 border-blue-400 bg-white focus:outline-none focus:ring-2 focus:ring-blue-400"
+        value={value}
+        placeholder="Enter Content Here"
+        onChange={(e) => setValue(e.target.value)}
+        rows={20}
+      />
     </div>
   );
 };
--- src/components/ViewPaste.jsx
+++ src/components/ViewPaste.jsx
@@ -28,18 +28,8 @@ const ViewPaste = () => {
           className={`w-full flex flex-col items-start relative rounded bg-opacity-10 border border-[rgba(128,121,121,0.3)] backdrop-blur-2xl`}
         >
           <div
-            className={`w-full rounded-t flex items-center justify-between gap-x-4 px-4 py-2 border-b border-[rgba(128,121,121,0.3)]`}
+            className={`w-full rounded-t flex items-center justify-end gap-x-4 px-4 py-2 border-b border-[rgba(128,121,121,0.3)]`}
           >
-            <div className="w-full flex gap-x-[6px] items-center select-none group">
-              <div className="w-[13px] h-[13px] rounded-full flex items-center justify-center p-[1px] overflow-hidden bg-[rgb(255,95,87)]" />
-
-              <div
-                className={`w-[13px] h-[13px] rounded-full flex items-center justify-center p-[1px] overflow-hidden bg-[rgb(254,188,46)]`}
-              />
-
-              <div className="w-[13px] h-[13px] rounded-full flex items-center justify-center p-[1px] overflow-hidden bg-[rgb(45,200,66)]" />
-            </div>
-            {/* Circle and copy btn */}
             <div
               className={`w-fit rounded-t flex items-center justify-between gap-x-4 px-4`}
             >
